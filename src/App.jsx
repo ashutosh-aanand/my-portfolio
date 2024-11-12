8fc8780bcc7aac5ac416aa97504ed70690dabb31
Ashutosh Anand <ashutosh.aanand@gmail.com>
2024-11-13 00:10:37 +0530

    updated page bg color

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Details from "./components/Details";
 
 export default function App() {
   return (
-    <div className="h-screen bg-slate-800 text-slate-400">
-      <div className="lg:flex gap-4 bg-slate-700 h-screen lg:px-24 lg:py-0">
+    <div className="h-screen bg-slate-900 text-slate-400">
+      <div className="lg:flex gap-4 h-screen lg:px-24 lg:py-0">
         <About/>
         <Details/>
       </div>
