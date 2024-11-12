8f617bbd165114be9e0c7d348f57b79eb35130a5
Ashutosh Anand <ashutosh.aanand@gmail.com>
2024-11-12 23:36:06 +0530

    fixed justify bet not working

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import { Icons } from '../Icons'
 
 export const About = () => {
   return (
-    <div className='w-1/2 py-24 max-h-screen flex-col justify-between'>
+    <div className='w-1/2 py-24 max-h-screen flex flex-col justify-between'>
       <div>
         
         <h1>Ashutosh Anand</h1>
